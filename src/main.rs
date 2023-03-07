1d7fa8b491d2e85f6880f340934f9b91dea1ec82
Oleg Gera <hi@oleggera.com>
2023-03-07 09:39:56 +0000

    chore: base connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        // A single stream represents an open connection between the client and the server.
+        let stream = stream.unwrap();
+        println!("Connection established!");
+        // When stream goes out of scope and is dropped at the end of the loop, the connection
+        // is closed as part of the drop implementation.
+    }
 }
