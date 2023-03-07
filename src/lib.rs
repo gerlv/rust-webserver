73937e94d153e09f2b00bd07f169fe13ab3504c9
Oleg Gera <hi@oleggera.com>
2023-03-07 10:39:29 +0000

    feat: add channels for sending jobs

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread
+};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender
         }
     }
 
@@ -23,7 +34,8 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
-
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -33,8 +45,12 @@ struct Worker {
 }
 
 impl Worker {
-    pub fn new(id: usize) -> Worker {
-        let thread = thread::spawn(||{});
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {id} got a job; executing.");
+            job();
+        });
 
         Worker {
             id,
